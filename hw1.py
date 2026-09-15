db874d9505fa7c9adbf2663f20e58e35cd7a7b1a
EGOR4ES <egor4escsgo@gmail.com>
2026-09-15 21:41:59 +0500

    Add files via upload

---
+++ hw1.py
@@ -0,0 +1,34 @@
+#1
+print("Hello, World!")
+
+import sys
+print(sys.version)
+
+#2
+# This is a comment.
+
+#3
+print("Python is fun!")
+
+print("Have a good day.")
+print("Learning Python is fun!")
+
+print("Hello"); print("How are you?"); print("Bye bye!")
+
+
+#4
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+#5
+a = "Hello"
+print(a)
+
+b = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(b)
+
+print(type(a))
